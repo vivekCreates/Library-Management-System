f79303c3f716b9e4fc0a0ec62301f0b01f269a0c
vivek <codecrafter67@gmail.compwd>
2025-08-05 15:39:50 +0530

    Refactor borrow book controller and routes; enhance error handling and ensure unique book/user retrieval

--- src/controllers/borrorBook.controller.js
+++ src/controllers/borrorBook.controller.js
@@ -6,96 +6,123 @@ import { ApiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import calculateBorrowBookReturnDate from "../utils/bookReturnDate.js";
 import calculateBorrowBookLoanAmount from "../utils/calculateLoanAmount.js";
-import checkCurrentUserAdminOrNot from "../utils/checkAdmin.js";
 
-const borrowBook = asyncHandler(async(req,res)=>{
-    const {id} = req.params;
-    const {duration} = req.body;
+const borrowBook = asyncHandler(async (req, res) => {
+  const { id: bookId } = req.params;
+  const { duration } = req.body;
 
-    if(!duration){
-        throw new ApiError(400,"Duration is required")
+  if (!duration) {
+    throw new ApiError(400, "Duration is required");
+  }
+  const alreadyBorrowBook = await BorrowRecord.findOne({
+    userId: req.user._id,
+    bookId:bookId,
+  });
+  if (alreadyBorrowBook) {
+    throw new ApiError(400, "Book already borrowed");
+  }
+
+  const { borrowDate, dueDate } = calculateBorrowBookReturnDate(duration);
+
+  const borrow = await BorrowRecord.create({
+    userId: req.user._id,
+    bookId,
+    borrowDate,
+    dueDate,
+  });
+
+  await Book.findByIdAndUpdate(
+    bookId,
+    {
+      $inc: {
+        availableCopies: -1,
+      },
+    },
+    {
+      new: true,
     }
-    const alreadyBorrowBook = await BorrowRecord.findOne({userId:req.user._id,bookId:id})
-    if(alreadyBorrowBook){
-        throw new ApiError(400,"Book already borrowed")
+  );
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { book: borrow }, "Book borrowed successfully"));
+});
+
+const returnBorrowedBook = asyncHandler(async (req, res) => {
+  const { id:bookId } = req.params;
+
+  const book = await Book.findOne({ _id: bookId });
+  console.log(book);
+  const record = await BorrowRecord.findOneAndUpdate(
+    { bookId, userId: req.user._id, status: bookStatusEnum.BORROWED },
+    {
+      status: bookStatusEnum.RETURNED,
+      loanAmount: calculateBorrowBookLoanAmount(book.dueDate, new Date()),
+    },
+    {
+      new: true,
     }
+  );
+  if (!record) {
+    throw new ApiError(404, "Book not found");
+  }
 
-    const {borrowDate,dueDate} = calculateBorrowBookReturnDate(duration);
-
-    const borrow = await BorrowRecord.create({
-        userId:req.user._id,
-        bookId:id,
-        borrowDate,
-        dueDate
-    })
-
-    return res.status(200).json(
-        new ApiResponse(
-            200,
-            {book:borrow},
-            "Book borrowed successfully"
-        )
-    )
-
-});
-
-const returnBorrowedBook = asyncHandler(async(req,res)=>{
-    const {id} = req.params;
-   
-    const book = await Book.findOne({_id:id});
-    console.log(book)
-    const record = await BorrowRecord.findOneAndUpdate({bookId:id,userId:req.user._id,status:bookStatusEnum.BORROWED},{
-        status:bookStatusEnum.RETURNED,
-        loanAmount:calculateBorrowBookLoanAmount(book.dueDate,new Date())
-    },{
-        new:true
+  await Book.findByIdAndUpdate(
+    bookId,
+    {
+      $inc: {
+        availableCopies:+1,
+      },
+    },
+    {
+      new: true,
     }
-);
-    if(!record){
-        throw new ApiError(404,"Book not found")
-    }
+  );
 
-    return res.status(200).json(
-        new ApiResponse(200,{book},"Book returned successfully")
-    )
-
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { book }, "Book returned successfully"));
 });
 
-const getBorrowBook = asyncHandler(async(req,res)=>{
-    const {id} = req.params;
+const getBorrowBook = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const book = await Book.findById(id);
 
-    const book = await Book.findById(id);
-
-    return res.status(200).json(
-        new ApiResponse(
-            200,
-            {book},
-            "Book fetch successfully"
-        )
-    )
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { book }, "Book fetch successfully"));
 });
 
-const getAllBorrowBooks = asyncHandler(async(req,res)=>{
-    checkCurrentUserAdminOrNot(req.user._id)
-    const books = await BorrowRecord.find().populate("bookId")
-    return res.status(200).json(
-        new ApiResponse(200,{books},"Borrow book fetch successfully")
-    )
+const getAllBorrowBooks = asyncHandler(async (req, res) => {
+  const bookRecords = await BorrowRecord.find({status:"BORROWED"}).populate("bookId");
+  const borrowedBooks = bookRecords.map(record => record.bookId);
+
+  const uniqueBooks = Array.from(
+    new Map(borrowedBooks.map(book => [book._id.toString(), book])).values()
+  );
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { books: uniqueBooks }, "Borrow book fetch successfully"));
+});
+
+const getAllUsersBorrowBooks = asyncHandler(async (req, res) => {
+  const userRecords = await BorrowRecord.find({status:"BORROWED"}).populate("userId");
+  const users = userRecords.map((user)=>user.userId);
+
+  const uniqueUsers = Array.from(
+    new Map(users.map((user)=>[user._id.toString(),user])).values()
+  )
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { users:uniqueUsers }, "Borrow book fetch successfully"));
 });
 
 
-const getAllUsersBorrowBooks = asyncHandler(async(req,res)=>{
-    checkCurrentUserAdminOrNot(req.user._id)
-    const books = await BorrowRecord.find().populate("userId")
-    return res.status(200).json(
-        new ApiResponse(200,{books},"Borrow book fetch successfully")
-    )
-});
 
 export {
-    borrowBook,
-    returnBorrowedBook,
-    getBorrowBook,
-    getAllBorrowBooks,
-    getAllUsersBorrowBooks
-}
+  borrowBook,
+  returnBorrowedBook,
+  getBorrowBook,
+  getAllBorrowBooks,
+  getAllUsersBorrowBooks,
+};
--- src/routes/borrowRecord.route.js
+++ src/routes/borrowRecord.route.js
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import { borrowBook, getAllBorrowBooks, getAllUsersBorrowBooks, getBorrowBook, returnBorrowedBook } from "../controllers/borrorBook.controller.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
-
+import authorizeRole from "../middlewares/authorizeRole.middleware.js";
 const router = Router();
 
-router.use(authMiddleware)
-router.route("/:id").post(borrowBook)
+// router.use(authMiddleware)
+router.route("/users").get(authMiddleware,authorizeRole,getAllUsersBorrowBooks)
+router.route("/books").get(authMiddleware,authorizeRole,getAllBorrowBooks)
+router.route("/:id").post(authMiddleware,authorizeRole,borrowBook)
 router.route("/return/:id").patch(returnBorrowedBook)
 router.route("/:id").get(getBorrowBook)
-router.route("/books").get(getAllBorrowBooks)
-router.route("/users").post(getAllUsersBorrowBooks)
 
 
 export default router
